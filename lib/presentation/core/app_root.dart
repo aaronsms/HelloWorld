82965426ed19c75a706a3c25e86cab32ed31ee3b
github-amanda <57126871+github-amanda@users.noreply.github.com>
2020-06-03 23:55:25 +0800

    preview page (completed booking)

--- lib/presentation/core/app_root.dart
+++ lib/presentation/core/app_root.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:helloworld/presentation/sign_in/sign_in_page.dart';
+// import 'package:helloworld/presentation/sign_in/sign_in_page.dart';
+import 'package:helloworld/presentation/preview/booking_system.dart';
 
 class AppRoot extends StatelessWidget {
   // This widget is the root of your application.
@@ -7,9 +8,8 @@ class AppRoot extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'HelloWorld!<>',
-      home: SignInPage(),
-      theme: ThemeData(
-      ),
+      home: BookingSystem(),
+      theme: ThemeData(),
     );
   }
 }
