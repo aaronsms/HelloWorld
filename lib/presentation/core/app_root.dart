8e1477c10d3373afd4b618330f548f922f2a771a
github-amanda <57126871+github-amanda@users.noreply.github.com>
2020-06-05 16:56:29 +0800

    completed pre-signup page
    alert dialog added

--- lib/presentation/core/app_root.dart
+++ lib/presentation/core/app_root.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:helloworld/presentation/sign_in/sign_in_page.dart';
+import 'package:helloworld/presentation/sign_up/sign_up_pre.dart';
 
 class AppRoot extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'HelloWorld!<>',
-      home: SignInPage(),
-      theme: ThemeData(
-      ),
+      home: SignUpPre(),
+      theme: ThemeData(),
     );
   }
 }
--- lib/presentation/core/palette.dart
+++ lib/presentation/core/palette.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 class Palette {
   static const Color primaryColor = Color(0xFF29335C);
+  static const Color secondaryColor = Color(0xFFF3A712);
+  static const Color tertiaryColor = Color(0xFFD6EFFF);
 }
