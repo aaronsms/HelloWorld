06597f3afae581c93d6f6020590e35c04d0c7aa7
github-amanda <57126871+github-amanda@users.noreply.github.com>
2020-06-10 16:59:19 +0800

    mentor account details draft

--- lib/presentation/sign_up/sign_up_account_mentor.dart
+++ lib/presentation/sign_up/sign_up_account_mentor.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:helloworld/presentation/core/palette.dart';
 import 'package:helloworld/presentation/sign_up/header.dart';
 
-class SignUpAccount extends StatefulWidget {
+class SignUpAccountMentor extends StatefulWidget {
   @override
-  _SignUpAccountState createState() => _SignUpAccountState();
+  _SignUpAccountMentorState createState() => _SignUpAccountMentorState();
 }
 
-class _SignUpAccountState extends State<SignUpAccount> {
+class _SignUpAccountMentorState extends State<SignUpAccountMentor> {
   bool _obscureText = true;
   final _name = TextEditingController();
   final _password = TextEditingController();
   final _confirm = TextEditingController();
   final _email = TextEditingController();
+  final _age = TextEditingController();
   bool _validateName = false;
   bool _validatePassword = false;
   bool _validateConfirm = false;
   bool _validateEmail = false;
+  bool _validateAge = false;
   // String _password;
 
   @override
@@ -36,14 +39,14 @@ class _SignUpAccountState extends State<SignUpAccount> {
     return Scaffold(
       appBar:
           PreferredSize(preferredSize: Size.fromHeight(80.0), child: Header()),
-      body: SizedBox(
+      body: SingleChildScrollView(
           child: Wrap(alignment: WrapAlignment.center, children: <Widget>[
         Container(
             padding: EdgeInsets.only(top: 15.0, bottom: 10.0),
             child: Image(
-              image: AssetImage('assets/images/learner_step1.png'),
-              height: 100,
-              width: 200,
+              image: AssetImage('assets/images/mentor_step1.png'),
+              height: 150,
+              width: 300,
             )),
         Container(
             padding: EdgeInsets.all(10.0),
@@ -207,7 +210,59 @@ class _SignUpAccountState extends State<SignUpAccount> {
                   ),
                 ))),
         Container(
-            padding: EdgeInsets.only(top: 20.0),
+            padding: EdgeInsets.all(10.0),
+            child: Theme(
+                data: ThemeData(
+                  primaryColor: Palette.primaryColor,
+                ),
+                child: TextFormField(
+                  controller: _age,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    WhitelistingTextInputFormatter.digitsOnly
+                  ],
+                  style: TextStyle(color: Palette.primaryColor),
+                  decoration: InputDecoration(
+                    errorText: _validateAge ? 'Field cannot be empty!' : null,
+                    prefixIcon: Icon(
+                      Icons.calendar_today,
+                      color: Palette.primaryColor,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius:
+                          const BorderRadius.all(const Radius.circular(15.0)),
+                      borderSide: BorderSide(
+                          color: _validateEmail
+                              ? Colors.red
+                              : Palette.primaryColor,
+                          style: BorderStyle.solid,
+                          width: 2),
+                    ),
+                    border: new OutlineInputBorder(
+                      borderRadius: const BorderRadius.all(
+                        const Radius.circular(15.0),
+                      ),
+                    ),
+                    contentPadding: EdgeInsets.all(10.0),
+                    labelText: "Age",
+                    labelStyle: TextStyle(
+                        color: Palette.primaryColor,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ))),
+        Container(
+            child: Text(
+                "Note: To be a mentor, you need to be at least 18 years old!",
+                style: TextStyle(
+                  height: 1.2,
+                  color: Colors.grey,
+                  fontWeight: FontWeight.w800,
+                  fontFamily: 'Martel Sans',
+                  fontSize: 12,
+                ))),
+        Container(
+            padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
             child: RaisedButton(
               color: Palette.primaryColor,
               shape: RoundedRectangleBorder(
