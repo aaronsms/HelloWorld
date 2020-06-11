cc6bbccca1e0ccdf8a7865b89a9af6da33659ecb
github-amanda <57126871+github-amanda@users.noreply.github.com>
2020-06-11 22:27:32 +0800

    password reset (first page)

--- lib/presentation/common/app_root.dart
+++ lib/presentation/common/app_root.dart
@@ -3,13 +3,15 @@ import 'package:helloworld/presentation/login/login_page.dart';
 import 'package:helloworld/presentation/preview/booking_system.dart';
 import 'package:helloworld/presentation/preview/messenger.dart';
 import 'package:helloworld/presentation/preview/rent_mentor.dart';
+import 'package:helloworld/presentation/passwordreset/submitemail.dart';
 
 class AppRoot extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'HelloWorld!<>',
-      home: MentorRenting(),
+      home: SubmitEmail(),
       theme: ThemeData(),
     );
   }
